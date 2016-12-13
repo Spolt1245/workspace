863e3e87e1ee4cebee5c819554e6a7a6cd6aea67
Steven Poltorak <spoltorak.sp@mysummitps.org>
2016-12-13 19:13:30 +0000

    message about changes

--- todo.js
+++ todo.js
@@ -3,6 +3,13 @@ var disp =  document.getElementById("disp");
 var btn =  document.getElementById("btn");
 
 btn.addEventListener("click", function(){
+
  disp.innerHTML+="<div>"+ inp.value+"</div>"
                        
-});            
+});                        
+
+disp.addEventListener("click",function(evt){
+  evt.target.parentNode.removeChild(evt.target); 
+  
+  
+})
