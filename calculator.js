f564289d421d7d55a175a9c45a96ad58f0beabd0
Steven Poltorak <spoltorak.sp@mysummitps.org>
2016-12-16 07:58:12 +0000

    message about changes

--- calculator.js
+++ calculator.js
@@ -1,17 +1,26 @@
+//These var tags create a variable. This makes it so the javascript can
 var num1input= document.getElementById("num1");
 var drop = document.getElementById("drop");
 var num2input= document.getElementById("num2");
 var solve= document.getElementById("solve");
 var display= document.getElementById("display");
-solve.addEventListener("click",function(){
+  
+//This solves the math problems, and waits for someone to click the button to solve
+
+solve.addEventListener("click",function(){  
+   
+ 
+    
+
+//The num1 and num2 are the imput values that allow you to do basic math problems
 var num1 = num1input.value;
 var num2 = num2input.value;   
-
+//These if else statements mean that if the dropdown bar is equal to a sign like +,-,/ etc.  
    if(drop.value === "+") {
        var answer = +num1 + +num2; 
        display.innerHTML = answer; 
    }
-   
+    
      else if (drop.value === "-") {
        var answe = num1 - num2; 
        display.innerHTML = answe; 
@@ -34,7 +43,7 @@ var num2 = num2input.value;
       display.innerHTML = a;
    }
    
-   
+ //The if else statements use math to solve math related questions.   
   
   
    
--- todo.js
+++ todo.js
@@ -1,15 +1,43 @@
-var inp =  document.getElementById("inp");
-var disp =  document.getElementById("disp");
-var btn =  document.getElementById("btn");
+//These var tags create a variable. This makes it so the javascript can do tasks    
+var inp = document.getElementById("inp");
+var disp = document.getElementById("disp");
+var btn = document.getElementById("btn");
+var textclr = document.getElementById("textclr");
 
-btn.addEventListener("click", function(){
+function myFunction() {
+    document.getElementById("myForm").reset(disp);
+}
+// This is an addEventListener which waits for a certain function to carry out the task. In this situation Click is a function that makes it so that when you press a button it will carry out the task 
+btn.addEventListener("click", function() {
 
- disp.innerHTML+="<div>"+ inp.value+"</div>"
-                       
-});                        
+//  disp.innerHTML += "<div>" + inp.value + "</div>"
 
-disp.addEventListener("click",function(evt){
-  evt.target.parentNode.removeChild(evt.target); 
-  
-  
+    // These are if else statements, if one thing is one way, then if it is else , than its equal to something else.  Each else if statment
+    // Each else if statment is changing the text color of the task, and inputting it into the page. It allows for changing certain aspects that are important  
+    if (textclr.value == "none") {
+        disp.innerHTML += '<div style="color:black;">' + inp.value + '</div>';
+    }
+    else if (textclr.value == "red") {
+        disp.innerHTML += '<div style="color:red">' + inp.value + '</div>';
+    }
+    else if (textclr.value == "blue") {
+        disp.innerHTML += '<div style="color:blue">' + inp.value + '</div>';
+    }
+    else if (textclr.value == "green") {
+        disp.innerHTML += '<div style="color:green">' + inp.value + '</div>';
+    }
+
+    else if (textclr.value == "aqua") {
+        disp.innerHTML += '<div style="color:aqua">' + inp.value + '</div>';
+    }
+    
+    else if (textclr.value == "white") {
+        disp.innerHTML += '<div style="color:white">' + inp.value + '</div>';
+    }
+});
+
+//
+disp.addEventListener("click", function(evt) {
+    evt.target.parentNode.removeChild(evt.target);
+//This line of code is used to target the thing being display, and removes it upon clicking on it. It is used because it is important remove things in a to do list
 })
